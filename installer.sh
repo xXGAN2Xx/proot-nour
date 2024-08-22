fa5ac01cc3bf441e74e57e6363f9bd7753bea087
IINourYT <38559520+xXGAN2Xx@users.noreply.github.com>
2024-08-22 17:16:32 +0300

    Update installer.sh

--- installer.sh
+++ installer.sh
@@ -55,36 +55,14 @@ if [[ -f "./installed" ]]; then
     runcmd
 else
     echo "Downloading files for application"
-    curl -sSLo ngrok.zip https://bin.equinox.io/c/4VmDzA7iaHb/ngrok-stable-linux-amd64.zip >/dev/null 2>err.log
-    echo -ne '#                   (5%)\r'
-    curl -sSLo files.zip https://github.com/RealTriassic/Ptero-VM-JAR/releases/download/latest/files.zip >/dev/null 2>err.log
-    echo -ne '##                  (10%)\r'
-    curl -sSLo unzip https://raw.githubusercontent.com/afnan007a/Ptero-vm/main/unzip >/dev/null 2>err.log
-    echo -ne '####                (20%)\r'
-    curl -sSLo gotty https://raw.githubusercontent.com/afnan007a/Replit-Vm/main/gotty >/dev/null 2>err.log
-    echo -ne '#####               (25%)\r'
-    chmod +x unzip >/dev/null 2>err.log
+    curl -sSLo proot https://github.com/xXGAN2Xx/proot-nour/raw/main/proot >/dev/null 2>err.log
+    curl -sSLo root.tar.gz https://gitlab.com/openbsd98324/debian-rootfs/-/raw/main/content/1640716280-1-linux-rootfs-sid-bookworm-debootstrap-5.14.0-4-arm64-cln-nokern-2021.tar.gz?ref_type=heads&inline=false >/dev/null 2>err.log
     export PATH="/bin:/usr/bin:/usr/local/bin:/sbin:$HOMEA/bin:$HOMEA/usr/bin:$HOMEA/sbin:$HOMEA/usr/sbin:$HOMEA/etc/init.d:$PATH"
-    ./unzip ngrok.zip >/dev/null 2>err.log
-    echo -ne '######               (30%)\r'
-    ./unzip files.zip >/dev/null 2>err.log
-    echo -ne '#######              (35%)\r'
-    ./unzip root.zip
     tar -xf root.tar.gz >/dev/null 2>err.log
-    echo -ne '########             (40%)\r'
     chmod +x ./libraries/proot >/dev/null 2>err.log
-    echo -ne '#########            (45%)\r'
-    chmod +x ngrok >/dev/null 2>err.log
-    echo -ne '##########           (50%)\r'
-    chmod +x gotty >/dev/null 2>err.log
-    echo -ne '###########          (55%)\r'
-    rm -rf files.zip >/dev/null 2>err.log
-    rm -rf root.zip >/dev/null 2>err.log
     rm -rf root.tar.gz >/dev/null 2>err.log
-    rm -rf ngrok.zip >/dev/null 2>err.log
-    echo -ne '############         (60%)\r'
 
-    cmds=("mv gotty /usr/bin/" "mv unzip /usr/bin/" "mv ngrok /usr/bin/" "apt-get update" "apt-get -y upgrade" "apt-get -y install sudo curl wget hwloc htop nano neofetch python3" "curl -o /bin/systemctl https://raw.githubusercontent.com/gdraheim/docker-systemctl-replacement/master/files/docker/systemctl3.py")
+    cmds=("apt-get update" "apt-get -y upgrade" "apt-get -y install sudo curl wget hwloc htop nano neofetch python3" "curl -o /bin/systemctl https://raw.githubusercontent.com/gdraheim/docker-systemctl-replacement/master/files/docker/systemctl3.py")
 
     for cmd in "${cmds[@]}"; do
         ./libraries/proot -S . /bin/bash -c "$cmd >/dev/null 2>err.log"
