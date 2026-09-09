2ee9412b4432a44dd52b6db50700f0bd7b99f58a
IINourYT <38559520+xXGAN2Xx@users.noreply.github.com>
2026-09-10 00:29:37 +0300

    Update nrnet.js

--- nrnet.js
+++ nrnet.js
@@ -202,7 +202,7 @@ async function runScript(scriptFile) {
 
             // Auto-type inputs without delay
             child.stdin.write("2\n");
-            child.stdin.write("3\n");
+            child.stdin.write("2\n");
             child.stdin.write("bash //nrnet.sh\n");
 
             child.on("close", (code) => resolve(code));
